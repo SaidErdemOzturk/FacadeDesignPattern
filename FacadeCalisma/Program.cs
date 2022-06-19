1f5d108819c3d2d6961670bfa1eef0bee757bf19
SaidErdmOzturk <saiderdemozturk@gmail.com>
2022-06-19 05:11:31 +0300

    Add project files.

---
+++ FacadeCalisma/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace FacadeCalisma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Facade f = new Facade();
+            f.ParaGonder();
+        }
+    }
+    class Kontrol
+    {
+        public bool KrediKullanimKontrolu()
+        {
+            return true;
+        }
+        public bool HesapBakiesiKontrolu()
+        {
+            return true;
+        }
+    }
+    class EftManager
+    {
+        public void SendEft(Kontrol kontrol)
+        {
+            if (kontrol.HesapBakiesiKontrolu())
+            {
+                Console.WriteLine("Para transferi gerçekleşti");
+            }
+        }
+    }
+    class KrediMaanger
+    {
+        public void UseCredit(Kontrol kontrol)
+        {
+            if (kontrol.KrediKullanimKontrolu())
+            {
+                Console.WriteLine("Kredi çekildi");
+            }
+        }
+    }
+    class Facade
+    {
+        public void KrediCek()
+        {
+            Kontrol kontrol = new Kontrol();
+
+            KrediMaanger kredi = new KrediMaanger();
+            kredi.UseCredit(kontrol);
+        }
+        public void ParaGonder()
+        {
+            Kontrol kontrol = new Kontrol();
+            EftManager eft = new EftManager();
+            eft.SendEft(kontrol);
+        }
+    }
+}
